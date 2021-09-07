4f22d8dd283d192d7ee7b3632929061aa7733f1f
delfindaniel <delfindaniel25@yahoo.com>
2021-09-07 09:21:20 +0100

    interacting with dom element

--- Js Intermediate/app.js
+++ Js Intermediate/app.js
@@ -82,5 +82,108 @@ function updateValue(first, second) {
 }
 
 updateValue(student1, person1);
+
+console.log(student1);
 console.log(person1.age);
 
+// function
+var scores = [76, 87, 98, 74, 59];
+
+function calculator(arr, fn) {
+    var resultArray = []
+    for (var index = 0; index < arr.length; index++) {
+        resultArray.push(fn(arr[index]));
+    }
+    return resultArray;
+}
+
+function isPassed(value) {
+    return value >= 70
+}
+
+var passes = calculator(scores, isPassed)
+console.log(passes)
+
+// i = (p*r*t) / 100
+function interestCalc(principal, rate, time) {
+    var interest = (principal * rate * time) / 100
+    return interest
+}
+var p = 5000;
+var r = 34;
+var t = 5;
+interestCalc(p, r, t);
+
+// functions returning functions
+// closures
+function jobPayment(language) {
+    if (language === "Javascript") {
+        return function(name) {
+        console.log(language);
+        console.log("Hey " + name + ", you will be paid N10000 per hour");
+    };
+    } else {
+        return function(name) {
+            console.log(language);
+            console.log("Hey, " + name + ". We owe you not!");
+        };
+    }
+}
+
+var jsJob = jobPayment( "Javascript");
+var javaJob = jobPayment("Java");
+
+jsJob("John")
+javaJob("Janet");
+
+jobPayment("Javascript")("Peter");
+jobPayment("Python")("Paulina");
+
+// IIFE - Immediately Invoked Function Expression
+function dice() {
+    var value = Math.floor(Math.random() * 6) + 1;
+    return value === 6;
+}
+console.log(dice());
+
+(function () {
+    var value = Math.floor(Math.random() * 6) + 1;
+    console.log(value === 6);
+})();
+
+(function(bet) {
+    var value = Math.floor(Math.random() * 6) + 1;
+    console.log(value === bet);
+})(4);
+
+// bind, call and apply
+var emmanuel = {
+    name: "Emmanuel",
+    course: "Architecture",
+    greeting: function(format, dayTime) {
+        if (format === "formal") {
+            console.log(
+                "Good " + dayTime + ", ladies and gentlemen. My name is " + this.name
+            );
+        } else {
+            console.log(
+                "Howdy, " + dayTime + ", guys and ladies. My name is " + this.name
+            );
+        }
+    }
+};
+
+var lifted = {
+    name: "Lifted",
+    course: "ICT"
+};
+emmanuel.greeting("informal", "morning");
+// lifted.greeting("formal", "afternoon");
+emmanuel.greeting.call(lifted, "formal", "evening");
+// emmanuel.greeting.apply(lifted, ["formal", "evening"]);
+
+var emmanuelInformal = emmanuel.greeting.bind(emmanuel, "informal");
+emmanuelInformal("afternoon");
+
+var liftedFormal = emmanuel.greeting.bind(lifted, "formal");
+liftedFormal("evening");
